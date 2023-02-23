57e95a306931518d9528f438fa4162dffa86d005
theekshanamadumal <66960247+theekshanamadumal@users.noreply.github.com>
2023-02-23 11:36:20 +0530

    added layouts

--- app/src/main/java/com/example/takeme/MainActivity.java
+++ app/src/main/java/com/example/takeme/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.takeme;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button capture, navigate;
+    OnClickListener cap = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(),
+                    MapActivity.class);
+            startActivity(intent);
+        }
+
+    };
+
+    OnClickListener nav = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(),
+                    FindPosition.class);
+            startActivity(intent);
+        }
+
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigate = (Button) findViewById(R.id.button);
+        capture = (Button) findViewById(R.id.button2);
+        navigate.setOnClickListener(nav);
+        capture.setOnClickListener(cap);
+
     }
 }
